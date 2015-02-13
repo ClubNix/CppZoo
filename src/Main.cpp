5e83da1966dddac9292500b7cd53aa327fc67b90
fmutix <fmutix@gmail.com>
2015-02-13 16:09:04 +0000

    draw map

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,21 +2,24 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+	sf::RenderWindow window(sf::VideoMode(480, 312), "Zoo");
+	sf::Texture backgroundTexture;
+	sf::Sprite background;
+	
+	backgroundTexture.loadFromFile("resources/map.png");
+	background.setTexture(backgroundTexture);
+	background.setOrigin(0.f,0.f);
 
-	// run the program as long as the window is open
-	while (window.isOpen())
-	{
-		// check all the window's events that were triggered since the last iteration of the loop
+	while (window.isOpen()){
 		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			// "close requested" event: we close the window
-			if (event.type == sf::Event::Closed)
+		while (window.pollEvent(event)){
+			if(event.type == sf::Event::Closed){
 				window.close();
+			}
 		}
 		
-		window.clear(sf::Color::Black);
+		window.clear();
+		window.draw(background);
 		window.display();
 	}
 
