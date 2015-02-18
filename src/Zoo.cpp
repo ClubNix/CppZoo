2cfc3a5cee79c7cdef0d0f9c7c1e4e61555497cc
nepta <nepta23@gmail.com>
2015-02-19 00:49:38 +0100

    used delegation for Zoo

--- src/Zoo.cpp
+++ src/Zoo.cpp
@@ -4,7 +4,11 @@ Zoo::Zoo(std::string fileName){
 	if (!texture_.loadFromFile(fileName)){
 		throw std::runtime_error("file not found");
 	}
-	setTexture(texture_);
-	setOrigin(0.f,0.f);	
+	background_.setTexture(texture_);
+	background_.setOrigin(0.f,0.f);	
+}
+
+void Zoo::draw(sf::RenderTarget& target, sf::RenderStates states) const{
+	target.draw(background_, states);
 }
 
--- src/Zoo.h
+++ src/Zoo.h
@@ -4,9 +4,17 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 
-class Zoo : public sf::Sprite{
+class Zoo : public sf::Drawable{
 	sf::Texture texture_;
-
+	sf::Sprite background_;
+	
+private:
+	/**
+	 * drawing function.
+	 * draw the Zoo
+	 */
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+	
 public:
 	Zoo(std::string fileName);
 };
