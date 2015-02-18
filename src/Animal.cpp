ad2e307877a8abded9b9bef8f455d57dd0eb7057
nepta <nepta23@gmail.com>
2015-02-18 06:19:55 +0100

    added bubble when animal talk

--- src/Animal.cpp
+++ src/Animal.cpp
@@ -1,4 +1,5 @@
 #include "Animal.h"
+#include <iostream>
 
 extern sf::Font font;
 
@@ -10,7 +11,7 @@ Animal::Animal(std::string path) : sprite_(path), text_("",font,20){
 
 void Animal::setPosition(sf::Vector2f position){
 	sprite_.setPosition(position);
-	text_.setPosition({position.x-10, position.y-20});
+	text_.setPosition({position.x-30, position.y-20});
 }
 
 void Animal::setAnimation(std::string animationName){
@@ -44,6 +45,23 @@ void Animal::update(sf::Time elapsedTime){
 void Animal::draw(sf::RenderTarget& target, sf::RenderStates states) const{
 	target.draw(sprite_, states);
 	if(isSayingSomething_){
+		//calculate position of text bubble
+		float textWidth = text_.getGlobalBounds().width;
+		float textHeight = text_.getGlobalBounds().height;
+		sf::CircleShape textBubble(textWidth/2);
+		textBubble.setOrigin(textWidth/2, textWidth/2);
+		sf::Vector2f textBubblePosition = text_.getPosition();
+		textBubblePosition.x += textWidth/2;
+		textBubblePosition.y += 0.9*textHeight; // have a better rendering
+		textBubble.setPosition(textBubblePosition);
+		
+		//kikoo bubble
+		textBubble.setFillColor(sf::Color(225,200,250));
+		textBubble.setOutlineThickness(1.5);
+		textBubble.setOutlineColor(sf::Color(125,100,150));
+		textBubble.setScale(1.6,1.6*textHeight/textWidth);
+		
+		target.draw(textBubble,states);
 		target.draw(text_,states);
 	}
 }
